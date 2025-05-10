81ea6d41e4f19a90e878648f339e83e46d050737
tehqua <154001571+tehqua@users.noreply.github.com>
2025-05-10 10:47:59 +0700

    Update 3_Training_model.py

--- retrain/3_Training_model.py
+++ retrain/3_Training_model.py
@@ -2,13 +2,10 @@ import logging
 import wandb
 import joblib
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
 
 # =========================================================================================================
 # 3. Training_model  
